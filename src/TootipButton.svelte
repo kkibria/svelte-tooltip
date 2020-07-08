33e37cb20029b798591e32112c53896352f0856a
Khan Kibria <khan.kibria@gmail.com>
2020-07-08 16:32:11 -0700

    update

--- src/TootipButton.svelte
+++ src/TootipButton.svelte
@@ -8,9 +8,11 @@
     leave: () => {},
   };
   export let style = "";
+  export let disabled = false;
+
 </script>
 
-<button {style}
+<button {style} {disabled}
   on:mouseover={handler.enter}
   on:mouseout={handler.leave}
   on:click={() => {
